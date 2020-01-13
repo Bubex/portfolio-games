cb69c5de4b7e9cc55c1b58d455868dde664e6aa5
Bubex <marlon2mrt@gmail.com>
2020-01-13 18:15:19 -0300

    update name

--- suitsys/sistema/_copyright.php
+++
@@ -1,9 +0,0 @@
-<!-- Footer -->
-<footer class="sticky-footer bg-white">
-     <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-               <span><?=date('Y')?> - <?=APP_NAME?> &copy; Todos os direitos reservados</span>
-          </div>
-     </div>
-</footer>
-<!-- End of Footer -->
--- suitsys/sistema/index.php
+++
@@ -1,3 +0,0 @@
-<?
-include 'session.php';
-header('Location:login.php');
